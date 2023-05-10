cf3b03673d8306e3ed7605cfddbba5cf0c6e1dab
Your Name <you@example.com>
2023-05-10 16:36:49 +0300

    fixes

---
+++ ConsoleApp1/Coin.cs
@@ -0,0 +1,26 @@
+﻿public class Coin
+{
+    public Side Side { get; private set; }
+    public Coin(Side side)
+    {
+        this.Side = side;        
+    }
+
+    internal void Flip()
+    {
+        if (this.Side == Side.gold)
+        {
+            this.Side = Side.silver;
+        }
+        else
+        {
+            this.Side = Side.gold;
+        }
+    }
+}
+
+public enum Side
+{
+    silver,
+    gold
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,3 @@
+﻿using ConsoleApp1;
+
+Game game = new Game();
